67b13d49a54c6c4c9bcef0a0ccedd625f661f588
Firman47 <122626748+Firman47@users.noreply.github.com>
2026-04-17 20:24:49 +0700

    feat: update CORS configuration to allow multiple origins

--- apps/api/main.go
+++ apps/api/main.go
@@ -21,9 +21,9 @@ func main() {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3001"}, 
-		AllowMethods:     []string{"GET,POST,PUT,DELETE,OPTIONS"},
-		AllowHeaders:     []string{"Origin,Content-Type,Accept,Authorization"},
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
 	}))
 
